eb3aad76bf5315a7550b997ab7e224a3bf180998
dailydevp <hyde8547@naver.com>
2021-07-23 13:45:48 +0900

    first

---
+++ src/main/java/org/zerock/service/MemberService.java
@@ -0,0 +1,22 @@
+package org.zerock.service;
+
+import org.zerock.domain.MemberVO;
+
+public interface MemberService {
+
+	boolean insert(MemberVO mem);
+
+	MemberVO read(String name);
+	
+	boolean modify(MemberVO mem);
+
+	boolean delete(MemberVO mem);
+	
+	boolean modify(MemberVO mem, String exPassword);
+
+	boolean delete(MemberVO mem, String exPassword);
+
+	boolean findid(MemberVO mem);
+
+	
+}
