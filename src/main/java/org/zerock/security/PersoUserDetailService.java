e7c6d9005724faf55acd8b37808f6eb2e25e57a8
dailydevp <hyde8547@naver.com>
2021-07-29 11:47:22 +0900

    auth not yet

---
+++ src/main/java/org/zerock/security/PersoUserDetailService.java
@@ -0,0 +1,23 @@
+package org.zerock.security;
+
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.zerock.domain.MemberVO;
+import org.zerock.mapper.MemberMapper;
+import org.zerock.security.domain.PersoUser;
+
+public class PersoUserDetailService implements  UserDetailsService{
+
+	private MemberMapper map;
+
+	@Override
+	public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+		MemberVO mem = map.read(username);
+
+		if(mem == null) {
+			throw new UsernameNotFoundException("사용자를 찾을 수 없습니다. nickname : "+username);
+		}
+		return new PersoUser(mem);
+	}
+}
